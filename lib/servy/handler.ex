dec75d22f5872ef73d34a6c043515f313198f8cd
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-03-09 17:01:35 -0600

    Add the resp_body in the request and make the response more dynamic

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,22 +13,20 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, reso_body: "" }
+    %{ method: method, path: path, resp_body: "" }
   end
 
-  def route(_conv) do
-    # TODO: Create a new map that also has the response body:
-    _conv = %{ method: "GET", path: "/wildthings", reso_body: "Bears, Lions, Tigers" }
+  def route(conv) do
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
-  def format_response(_conv) do
-    # TODO: Use values in the map to create an HTTP response string:
+  def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
