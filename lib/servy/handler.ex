7efee532dd514b30cb2d25201bf322c1483c9bba
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-03-14 23:03:14 -0600

    Add request with id and create the route

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -14,7 +14,11 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{  method: method, 
+        path: path, 
+        resp_body: "",
+        status: nil
+    }
   end
 
   def log(conv), do: IO.inspect conv
@@ -24,21 +28,40 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+        200 => "OK",
+        201 => "Created",
+        401 => "Unauthorized",
+        403 => "Forbidden",
+        404 => "Not Found",
+        500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -57,6 +80,32 @@ IO.puts "**************"
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+Uer-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts "**************"
+IO.puts response
+IO.puts "**************"
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+Uer-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts "**************"
+IO.puts response
+IO.puts "**************"
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 Uer-Agent: ExampleBrowser/1.0
 Accept: */*
