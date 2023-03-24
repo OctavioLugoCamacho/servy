6cbdc171e7ed21c07708e9b71e21e9f3d8860d5a
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-03-24 10:04:38 -0600

    Add moduledoc and the global pages_path

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,10 @@
 defmodule Servy.Handler do
+
+  @moduledoc "Handles HTTP requuests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     request
     |> parse
@@ -48,7 +54,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-      Path.expand("../../pages", __DIR__)
+      @pages_path
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
@@ -70,6 +76,7 @@ defmodule Servy.Handler do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
+  @doc "Logs 404 requests."
   def track(%{status: 404, path: path} = conv) do
     IO.puts "Warning: #{path} is on the loose!"
     conv
