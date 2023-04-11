bff5d4ed9222bd5439b470e8a16096f4bbf49baa
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-11 14:43:59 -0600

    Add the render function to pass the templates and the bindings

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,28 +3,32 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  @templates_path Path.expand("../../templates", __DIR__)
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+    
+    %{ conv | status: 200, resp_body: content }
   end
 
   def index(conv) do
-    items = 
+    bears = 
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join
-
-    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
+    
+      render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>" }
+    render(conv, "show.eex", bear: bear)
   end
 
-  def create(conv, %{"name" => name, "type" => type} = params) do
+  def create(conv, %{"name" => name, "type" => type} = _params) do
     %{ conv | status: 201, 
               resp_body: "Created a #{type} bear named #{name}!" }
   end
