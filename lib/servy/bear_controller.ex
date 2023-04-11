768b71fc5ccc74eb57215408b014d7662c760e24
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-10 19:21:45 -0600

    Create the Bear Controller with the functions

---
+++ lib/servy/bear_controller.ex
@@ -0,0 +1,14 @@
+defmodule Servy.BearController do
+  def index(conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def show(conv, %{"id" => id}) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def create(conv, %{"name" => name, "type" => type} = params) do
+    %{ conv | status: 201, 
+              resp_body: "Created a #{type} bear named #{name}!" }
+  end
+end
