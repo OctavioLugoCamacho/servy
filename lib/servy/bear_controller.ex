96d1b55aae519224a608077caf7a70facb7f9146
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-11 12:43:17 -0600

    Add the functions to get the bears by id (integer and binary)

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,14 +1,27 @@
 defmodule Servy.BearController do
 
   alias Servy.Wildthings
+  alias Servy.Bear
+
+  defp bear_item(bear) do
+    "<li>#{bear.name} - #{bear.type}</li>"
+  end
 
   def index(conv) do
-    bears = Wildthings.list_bears()
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+    items = 
+      Wildthings.list_bears()
+      |> Enum.filter(&Bear.is_grizzly/1)
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+      |> Enum.map(&bear_item/1)
+      |> Enum.join
+
+    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
   end
 
   def show(conv, %{"id" => id}) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+    bear = Wildthings.get_bear(id)
+
+    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>" }
   end
 
   def create(conv, %{"name" => name, "type" => type} = params) do
