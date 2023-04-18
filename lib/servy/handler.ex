89ecc2c5a0ffc0c7c2cc2a010989eda56bd309f1
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-18 10:56:52 -0600

    Add the route for /api/bears and replace the resp_content_type in the format_response

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -29,6 +29,10 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
+  def route(%Conv{method: "GET", path: "/api/bears"} = conv) do
+    Servy.Api.BearController.index(conv)
+  end
+
   def route(%Conv{method: "GET", path: "/bears"} = conv) do
     BearController.index(conv)
   end
@@ -68,7 +72,7 @@ defmodule Servy.Handler do
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: text/html\r
+    Content-Type: #{conv.resp_content_type}\r
     Content-Length: #{String.length(conv.resp_body)}\r
     \r
     #{conv.resp_body}
