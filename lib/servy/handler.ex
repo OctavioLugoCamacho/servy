a43be0e1d8ecb8a200aa09405fa4c0d5bf0ec2e1
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-19 10:26:00 -0600

    Add the route for camera using spawns and receives functions

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,6 +4,7 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -21,11 +22,23 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  # def route(conv) do
-    # route(conv, conv.method, conv.path)
-  # end
+  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
+    parent = self() # Request-handling process
 
-  def route(%Conv{method: "GET", path: "/kaboom"} = _conv) do
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
+
+    snapshot1 = receive do {:result, filename} -> filename end
+    snapshot2 = receive do {:result, filename} -> filename end
+    snapshot3 = receive do {:result, filename} -> filename end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{ conv | status: 200, resp_body: inspect snapshots }
+  end
+
+  def route(%Conv{method: "GET", path: "/kaboom"}) do
     raise "Kaboom!"
   end
 
