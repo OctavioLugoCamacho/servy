f29f111f42dd267e9a896f5b783aeaea2f369a93
Octavio Lugo Camacho <octavio@makingdevs.com>
2023-04-20 11:17:01 -0600

    Add the Pledge Server

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,3 +1,20 @@
 defmodule Servy.PledgeServer do
 
+  def create_pledge(name, amount) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+
+    # Cache the pledge:
+    [ {"larry", 10} ]
+  end
+
+  def recent_pledges do
+    # Returns the most recent pledges(cache):
+    [ {"larry", 10} ]
+  end
+
+  defp send_pledge_to_service(_name, _amount) do
+    # Code to send pledge to external service
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+
 end
